24095fd8635357c1700c89aed9e23f4e45366f5b
HuyTQ <c0811g0911@gmail.com>
2018-09-25 15:12:19 +0700

    change something

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import Article from '../Article';
-import Pagination from '../Pagination';
+import Article from "../Article";
+import Pagination from "../Pagination";
 
 const Welcome = () => {
-    return (
-        <main className="main-content bg-gray">
-            <div className="row">
-                <div className="col-12 col-lg-6 offset-lg-3">
-                    <Article />
+  return (
+    <main className="main-content bg-gray">
+      <div className="row">
+        <div className="col-12 col-lg-6 offset-lg-3">
+          <Article />
 
-                    <hr />
-                    <Article />
+          <hr />
+          <Article />
 
-                    <hr />
-                    <Article />
+          <hr />
+          <Article />
 
-                    <hr />
-                    <Article />
+          <hr />
+          <Article />
 
-                    <Pagination />
-                </div>
-            </div>
-        </main>
-    )
-}
+          <Pagination />
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default Welcome;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter,Route} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter, Route, withRouter } from "react-router-dom";
 
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import Welcome from './components/Welcome';
-import Footer from './components/Footer';
-import Login from './components/Login';
-import Register from './components/Register';
-import SingleArticle from './components/SingleArticle';
-import CreateArticle from './components/CreateArticle';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Welcome from "./components/Welcome";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import SingleArticle from "./components/SingleArticle";
+import CreateArticle from "./components/CreateArticle";
+
+const Main = withRouter(location => {
+  console.log("location", location);
+  return (
+    <div>
+      {location.location.pathname !== "/login" &&
+        location.location.pathname !== "/register" && <Navbar />}
+      <Header/>
+      <Route exact path="/" component={Welcome} />
+      <Route path="/articles/create" component={CreateArticle} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="/article/:id" component={SingleArticle} />
+      {location.location.pathname !== "/login" &&
+        location.location.pathname !== "/register" && <Footer />}
+    </div>
+  );
+});
 
 ReactDOM.render(
-    
-        <BrowserRouter>
-            <div>
-                <Navbar />
-                <Header />
-                <Route exact path='/' component={Welcome}/>                
-                <Route path='/articles/create' component={CreateArticle}/>
-                <Route path='/login' component={Login}/>
-                <Route path='/register' component={Register}/>
-                <Route path='/article/:id' component={SingleArticle}/>
-                <Footer/>
-            </div>
-        </BrowserRouter>
-    , document.getElementById('root'));
+  <BrowserRouter>
+    <Main />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 registerServiceWorker();
